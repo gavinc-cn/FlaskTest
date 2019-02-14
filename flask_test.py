5cc44aa87ca3de7fafa7447cf399684f48e2422b
gavinc <gavincwork@126.com>
2019-02-14 18:55:00 +0800

    mirror

--- flask_test.py
+++ flask_test.py
@@ -1,10 +1,23 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello():
     return "Hello World!"
+
+
+def values2dict(content):
+    _dict = {}
+    for i in content:
+        _dict[i] = content[i]
+    return _dict
+
+
+@app.route("/mirror", method=['GET', "POST"])
+def mirror():
+    content = values2dict(request.values)
+    return jsonify(content)
 
 
 if __name__ == "__main__":
